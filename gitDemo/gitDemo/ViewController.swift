b68c6065154d51bc84f2515a86c0a572055612b6
ArtemVahrin <artemvahrin6@gmail.com>
2025-11-11 13:10:52 +0300

    Initial commit

---
+++ gitDemo/gitDemo/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  gitDemo
+//
+//  Created by Артём on 29.10.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("Another print statement")
+        print("Master or main branch print")
+    }
+
+
+}
+
